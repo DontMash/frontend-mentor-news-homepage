185ea1424d3d31fd57cff1d5d95cc49b46d8c8b6
Sören Maschmann <smaschmann@slashwhy.de>
2022-10-29 01:24:54 +0200

    Done

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,28 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        paragraph: '0.9375rem',
+      },
+    },
+    colors: {
+      "soft-orange": "#e9ab53",
+      "soft-red": "#f15e50",
+
+      "off-white": "#fffdfa",
+      "grayish-blue": "#c5c6ce",
+      "dark-grayish-blue": "#5d5f79",
+      "very-dark-blue": "#00001a",
+    },
+    fontFamily: {
+      "inter": ['Inter', 'sans-serif'],
+      "inter-variable": ['Inter Variable', 'sans-serif'],
+    },
+    screens: {
+      sm: '375px',
+      xl: '1440px' 
+    }
   },
   plugins: [],
 };
