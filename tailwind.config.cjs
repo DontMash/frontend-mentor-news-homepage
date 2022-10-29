9ebb4f1782f07b0daa0d74dccc923e5af0538179
Sören Maschmann <smaschmann@slashwhy.de>
2022-10-29 02:49:06 +0200

    fix: accessibility & html errors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,21 +8,23 @@ module.exports = {
       },
     },
     colors: {
-      "soft-orange": "#e9ab53",
-      "soft-red": "#f15e50",
+      'transparent': 'transparent',
 
-      "off-white": "#fffdfa",
-      "grayish-blue": "#c5c6ce",
-      "dark-grayish-blue": "#5d5f79",
-      "very-dark-blue": "#00001a",
+      'soft-orange': '#e9ab53',
+      'soft-red': '#f15e50',
+
+      'off-white': '#fffdfa',
+      'grayish-blue': '#c5c6ce',
+      'dark-grayish-blue': '#5d5f79',
+      'very-dark-blue': '#00001a',
     },
     fontFamily: {
-      "inter": ['Inter', 'sans-serif'],
-      "inter-variable": ['Inter Variable', 'sans-serif'],
+      'inter': ['Inter', 'sans-serif'],
+      'inter-variable': ['Inter Variable', 'sans-serif'],
     },
     screens: {
       sm: '375px',
-      xl: '1440px' 
+      xl: '1440px'
     }
   },
   plugins: [],
